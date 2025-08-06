6f8bddbe502c6ba0d4826dda669603bea62af542
unknown <ztatyana1995@yandex.ru>
2025-08-06 17:46:05 +0300

    переместили методы перехода по ссылке логина в base page и добавили эти тесты на product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 class BasePage():
     def __init__(self, browser, url, timeout=10): # Здесь timeout=10 — это значение по умолчанию, т.е если ты не укажешь timeout при создании объекта, он автоматически будет равен 10
         self.browser = browser
@@ -51,3 +52,10 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
