c54a082ff3f7833ce320b35231c3e44e1d5d565d
Татьяна <ztatyana1995@yandex.ru>
2025-08-13 11:53:10 +0300

    поправила метод проверки

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,14 +13,12 @@ class ProductPage(BasePage):
     def correct_message_about_adding_to_cart(self): #проверяем что текст сообщения в плашке корректный
         title = self.browser.find_element(*ProductPageLocators.BOOK_TITLE).text
         message_about_adding_to_cart = self.browser.find_element(*ProductPageLocators.MESSAGE_TEXT_ABOUT_ADDING_TO_CART).text
-        expected_text = f"{title} был добавлен в вашу корзину."
-        assert expected_text in message_about_adding_to_cart, f"Ожидали '{expected_text}', но получили '{message_about_adding_to_cart}'"
+        assert title in message_about_adding_to_cart, f"В сообщении '{message_about_adding_to_cart}' нет названия товара '{title}'"
 
     def correct_message_about_the_cart_cost(self): #проверяем что стоимость корзины в плашке указана корректно
         price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         message_about_adding_the_cart_cost = self.browser.find_element(*ProductPageLocators.MESSAGE_TEXT_ABOUT_THE_CART_COST).text
-        expected_text = f"Стоимость корзины теперь составляет {price}"
-        assert expected_text in message_about_adding_the_cart_cost, f"Ожидали '{expected_text}', но получили '{message_about_adding_the_cart_cost}'"
+        assert price in message_about_adding_the_cart_cost, f"В сообщении '{message_about_adding_the_cart_cost}' должна быть указана цена '{price}'"
 
     def success_message_is_disappeared(self): #проверка, что сообщение исчезло (тест должен падать, так как сообщение не должно исчезать)
         assert self.is_disappeared(*ProductPageLocators.MESSAGE_TEXT_ABOUT_ADDING_TO_CART), \
